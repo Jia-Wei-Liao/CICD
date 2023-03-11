8ab5ace84a872f55a197d1c997ea1b9e31f51cab
Jia-Wei-Liao <sam23582211@gmail.com>
2023-03-11 09:06:35 +0800

    First commit

---
+++ train.py
@@ -0,0 +1,15 @@
+from sklearn.ensmble import RandomForestClassifer
+from sklearn.datasets import load_breast_cancer, load_iris
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+
+dataset = data_load_iris(as_frame=True)
+X = dataset.data.to_numpy()
+y = dataset.target.values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+model = RandomForestClassifer()
+model.fit(X_train, y_train)
+acc = model.score(X_test, y_test)
+print(f"test acc: {acc}")
